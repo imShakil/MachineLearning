a8ed07274456a3fa3512048f53148966cda7aba8
mhiceiuk <mh.ice.iu@gmail.com>
2019-10-12 17:01:11 +0600

    Initial commit

---
+++ hello.py
@@ -0,0 +1,12 @@
+"""
+Mobarak Hosen Shakil
+ICT, Islamic University
+Kushtia - 7003, Bangladesh
+
+File: hello.py
+Project: MachineLearning
+Created: 11:16 AM, 9/18/2019
+
+"""
+
+print("Hello World")

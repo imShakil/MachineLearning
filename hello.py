30548bb3f3775c2460bc79e7e7114c2073e90bc4
mhiceiuk <mh.ice.iu@gmail.com>
2019-10-19 16:41:37 +0600

    Updated

--- hello.py
+++ hello.py
@@ -9,4 +9,26 @@ Created: 11:16 AM, 9/18/2019
 
 """
 
-print("Hello World")
+import pandas
+import numpy
+import cv2
+import tensorflow
+import keras
+import gym
+import scipy
+import matplotlib
+import theano
+import seaborn
+import sklearn
+
+print("Pandas Version: {}".format(pandas.__version__))
+print("Numpy Version: {}".format(numpy.__version__))
+print("Opencv Version: {}".format(cv2.__version__))
+print("Keras Version: {}".format(keras.__version__))
+print("Tensorflow Version: {}".format(tensorflow.__version__))
+print("Gym Version: {}".format(gym.__version__))
+print("Matplotlib Version: {}".format(matplotlib.__version__))
+print("Scipy Version: {}".format(scipy.__version__))
+print("Theano Version: {}".format(theano.__version__))
+print("Seaborn Version: {}".format(seaborn.__version__))
+print("Sklearn Version: {}".format(sklearn.__version__))
